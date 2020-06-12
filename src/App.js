b10d36dd184716e73128f261c6ee9e1a6ae9ff45
Matthews Ma <matthewsma21@gmail.com>
2020-06-12 15:09:41 -0400

    Configure anonymous login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+
+import * as firebase from "firebase";
 import "firebase/auth";
+import * as firebaseui from "firebaseui";
 
 import logo from './logo.svg';
 import './App.css';
@@ -8,18 +11,73 @@ import Navbar from "./components/navbar";
 import ChatApp from "./components/chatapp";
 
 function App() {
+    // Your web app's Firebase configuration
     const firebaseConfig = {
-
+        apiKey: "AIzaSyCrnkoSLbcPCfn0K2H7ym_rwCqU2ztSoWU",
+        authDomain: "chat-app-86e50.firebaseapp.com",
+        databaseURL: "https://chat-app-86e50.firebaseio.com",
+        projectId: "chat-app-86e50",
+        storageBucket: "chat-app-86e50.appspot.com",
+        messagingSenderId: "807312093511",
+        appId: "1:807312093511:web:213506117209fe7bea1c62",
+        measurementId: "G-R7Z40830FQ"
     };
+    firebase.initializeApp(firebaseConfig);
+
+    // // Firebase UI
+    // const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // ui.start('#firebaseui-auth-container', {
+    //     callbacks: {
+    //         signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+    //             // User successfully signed in.
+    //             // Return type determines whether we continue the redirect automatically
+    //             // or whether we leave that to developer to handle.
+    //             return true;
+    //         },
+    //         uiShown: function() {
+    //             // The widget is rendered.
+    //             // Hide the loader.
+    //             document.getElementById('loader').style.display = 'none';
+    //         }
+    //     },
+    //     // Terms of service url.
+    //     tosUrl: '<your-tos-url>',
+    //     // Privacy policy url.
+    //     privacyPolicyUrl: '<your-privacy-policy-url>'
+    // });
+
+    function anonymousAuth() {
+        firebase.auth().signInAnonymously().catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+        });
+    }
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            console.log(uid);
+            // ...
+        } else {
+            // User is signed out.
+            // ...
+        }
+        // ...
+    });
 
     const loggedIn = true;
     const showChat = false;
 
     return (
         <div className="App">
+            <button className="btn btn-primary mt-5 pt-5" onClick={anonymousAuth}>hiof</button>
             <Navbar/>
             {loggedIn ? "" : <Login/>}
-            {showChat ? "" : <ChatApp/>}
+            {showChat ? <ChatApp/> : ""}
         </div>
     );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -4,11 +4,15 @@ import './login.css';
 function Login() {
     return (
         <form className="form-signin text-center">
-            <h1 className="h2 font-weight-normal mb-4">Please Log In</h1>
-            <input type="username" id="inputUsername" placeholder="Username" className="form-control" />
-            <input type="password" id="inputPassword" placeholder="Password" className="form-control mb-4" />
-            <button className="btn btn-lg btn-primary btn-block">Log In</button>
-            <p className="mt-5 mb-3 text-muted">© 2020</p>
+            <h1>Welcome to My Awesome App</h1>
+            <div id="firebaseui-auth-container"/>
+            <div id="loader">Loading...</div>
+
+            {/*<h1 className="h2 font-weight-normal mb-4">Please Log In</h1>*/}
+            {/*<input type="username" id="inputUsername" placeholder="Username" className="form-control" />*/}
+            {/*<input type="password" id="inputPassword" placeholder="Password" className="form-control mb-4" />*/}
+            {/*<button className="btn btn-lg btn-primary btn-block">Log In</button>*/}
+            {/*<p className="mt-5 mb-3 text-muted">© 2020</p>*/}
         </form>
     )
 }
