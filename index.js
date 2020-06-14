065afa37adb80ea306d892373d451661477e33ca
Rohan Shetty <rshetty449@gmail.com>
2020-06-14 02:09:24 +0000

    added chat system

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ let db = admin.firestore();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Use correct file directory
+/* POST Request Body Example:
+{
+	email: 'shetr7624@wrdsb.ca',
+	username: 'rohaniscool',
+	type: 'user' 				['user' or 'volunteer']
+}
+*/
 app.post('/registerUser', (req, res) => {
 	if (req.body.email == null){
 	} else {
@@ -47,6 +53,9 @@ app.post('/registerUser', (req, res) => {
 					supporters: new Array(),
 					supporting: new Array(),
 					groups: new Array(),
+					type: req.body.type,
+					topics: new Array(),
+					chats: new Array(),
 				};
 
 				// Add a new document in collection 'users' with ID user email
@@ -71,11 +80,95 @@ app.post('/registerUser', (req, res) => {
 		});
 	}
 });
- 
-app.get('/getUser', (req, res) => {
-	let data = db.collection('users').doc(req.body.username).get().then((name) => {
-		res.write(data);
+
+
+/* POST Request Body Example
+{
+	username: 'rohaniscool'
+}
+*/
+app.post('/findVolunteer', (req, res) => {
+	db.collection('users').doc(req.body.username).get().then(doc => {
+		doc = doc.data();
+		let topics = doc.topics;
+		let matchedVolunteers = {};
+		db.collection('users').where('type', '==', 'volunteer').get().then(snapshot => {
+			snapshot.forEach(volunteer => {
+				matchedVolunteers[volunteer.id] = topics.filter(el => {
+																						return volunteer.data().topics.indexOf(el) >= 0;
+																					}).length;
+			});
+			let largest = Math.max(Object.values(matchedVolunteers));
+			result = Object.keys(matchedVolunteers).reduce((result, key) => {
+				if (matchedVolunteers[key] === largest) { 
+					result.push(key);
+				} 
+				return result;
+			}, [])
+			res.end(result[Math.floor(Math.random() * (result.length - 1))]);
+		});
 	});
+});
+
+/* POST Request Body Example
+{
+	user1: 'rohaniscool',
+	user2: 'matthewsisawesome'
+}
+*/
+app.post('/confirmChat', (req, res) => {
+	db.collection('users').doc(req.body.user1).update({chats: admin.firestore.FieldValue.arrayUnion(req.body.user2)});
+	db.collection('users').doc(req.body.user2).update({chats: admin.firestore.FieldValue.arrayUnion(req.body.user1)});
+	res.sendStatus(200);
+});
+
+/* POST Request Body Example
+{
+	username: 'rohaniscool',
+	topics: ['depression', 'anxiety', 'ocd']
+}
+*/
+app.post('/updateTopics', (req, res) => {
+	let updateTopics = db.collection('users').doc(req.body.username).update({topics: req.body.topics});
+	res.sendStatus(200);
+});
+
+/* POST Request Body Example
+{
+	user1: 'rohaniscool',
+	user2: 'matthewsisawesome'
+}
+*/
+app.post('/getChatHistory', (req, res) => {
+	db.collection('chats').doc(req.body.user1 + '++' + req.body.user2).get().then(doc => {
+		res.end(doc.data().messages);
+	});
+});
+
+/* POST Request Body Example
+{
+	sender: 'rohaniscool',
+	receiver: 'matthewsisawesome',
+	message: 'whats up my chigerian'
+}
+*/
+app.post('/newChatMessage', (req, res) => {
+	let messageUsers = [req.body.sender, req.body.receiver];
+	messageUsers.sort();
+	let queryString = messageUsers[0] + '++' + messageUsers[1];
+	let time = new Date().getTime();
+	db.collection('chats').doc(queryString).update(
+		{
+			messages: admin.firestore.FieldValue.arrayUnion(
+				{
+					sender: req.body.sender,
+					timestamp: new admin.firestore.Timestamp(Math.floor(time / 1000), (time % 1000) * 1000000),
+					message: req.body.message
+				}
+			)
+		}
+	)
+	res.sendStatus(200);
 });
 
 server.listen(3000, () => {
